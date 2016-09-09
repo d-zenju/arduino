d87a83b1551dc40381ab51cb7e62e497ca51bb00
d-zenju <yoshizumi@zoho.com>
2016-09-09 16:47:26 +0900

    fix servo.ino

--- servo/servo.ino
+++ servo/servo.ino
@@ -5,7 +5,10 @@ Purpose: Servo Control by Serial
 
 #include <Servo.h>
 
+// servo
 Servo servo;
+int flag = -1;
+int sum = 0;
 int deg = 0;
 
 void setup() {
@@ -18,7 +21,37 @@ void setup() {
   Serial.println("Ready");
 }
 
+void readSerial() {
+  char str;
+  if (Serial.available()) {
+    str = Serial.read();
+    
+    switch (str) {
+      case '0'...'9':
+        sum = sum * 10 + (str - 48);
+        break;
+        
+      case 's':
+        deg = sum;
+        flag = 1;
+        Serial.println(deg);
+        sum = 0;
+        break;
+        
+      default:
+        sum = 0;
+        break;
+    }
+  }
+}
+
 void loop() {
+  // read Serial (ex. 180deg --> 180s)
+  readSerial();
+  
   // move Servo
-  servo.write(deg);
+  if (flag != -1) {
+    servo.write(deg);
+    flag = -1;
+  }
 }
